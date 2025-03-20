a5e2dcc81c719a1cec574ebe14c662a48b60c0fe
arun prabu <arunprabuselvaraj@gmail.com>
2025-03-20 19:39:33 +0530

    Initial commit

---
+++ cmd/api/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"santcum/internal/webserver"
+)
+
+func main() {
+	fmt.Println("Hello World, I'm working on starting the web server")
+	webserver.StartServer()
+}
